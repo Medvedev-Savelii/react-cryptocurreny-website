8e1facfeba193e819cc3e8b9ab9f9686ebc79fc1
sava <savamedvedev@gmail.com>
2022-05-04 20:27:47 +0800

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Featured from "./components/featured/Featured";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Featured />
     </>
   );
 }
---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import BTC from "../../assets/btc-img.png";
+import { FiArrowUpRight, FiArrowDown } from "react-icons/fi";
+import "./Featured.css";
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="container">
+        <div className="left">
+          <h2>Explore top Crypto's Like Bitcoin, Ethereum, and Dogecoin</h2>
+          <p>See all available assets: Cryptocurrencies and NFT's</p>
+          <button className="btn">See More Coins</button>
+        </div>
+
+        <div className="right">
+          <div className="card">
+            <div className="top">
+              <img src={BTC} alt="/" />
+            </div>
+            <div>
+              <h5>Bitcoin</h5>
+              <p>$49,000</p>
+            </div>
+            <span>
+              <FiArrowUpRight />
+              2.5%
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
