5f832740d30b50e672830f59f2c939b7854e65de
sava <savamedvedev@gmail.com>
2022-05-04 18:57:35 +0800

    first-commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <h1>
+          De<span className="primary">Fi</span>
+        </h1>
+        <ul className="nav-menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Featured</a>
+          </li>
+          <li>
+            <a href="/">Earn</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group">
+          <div className="btn">Connect Wallet</div>
+        </div>
+        <div className="hamburger">
+          <FaBars size={20} style={{ color: "#333" }} className="icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
